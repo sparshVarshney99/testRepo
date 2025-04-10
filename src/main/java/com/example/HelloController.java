491b29b24923ea52a535225728ae52bc985c4410
Sparsh Varshney <sparshvarshney@Dikshas-MacBook-Pro.local>
2025-04-10 18:03:24 +0530

    change

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, World!! Kaise ho ssabb";
+        return "Hello, World!! Kaise ho sabb";
     }
 }
